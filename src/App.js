5f492f2bb76531b454c66bd7295d79b21b609896
Pasanbasuru <basurupasan@gmail.com>
2020-03-26 11:43:44 +0530

    Integrate Asian statistical chart (#15)
    * Integrate Asian statistical chart
    
    Co-authored-by: Dhanushka C <dhanushka787@gmail.com>

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import moment from "moment";
 import DistrictPanel from "./Pages/DistrictPanel/DistrictPanel";
 import DistrictMapPanel from "./Pages/DistrictMapPanel/DistrictMapPanel";
 import ChartPanel from "./Pages/ChartPanel/ChartPanel";
+import ASIAN_COUNTRIES from "./utils/AsianCountries";
 
 const {Header, Content, Footer} = Layout;
 const {Title, Text} = Typography;
@@ -45,6 +46,10 @@ function App() {
     const [countriesReported, setCountriesReported] = useState(0);
     const [ageChartData, setAgeChartData] = useState([]);
     const [patientDataUpdatedAt, setPatientDataUpdatedAt] = useState("");
+    const [isAsia, setIsAsia] = useState(false);
+    const [defaultCounties] = useState(["Malaysia", "Sri Lanka", "India", "Pakistan", "Singapore", "Japan"]);
+    const [filteredCountries, setFilteredCountries] = useState([]);
+    const [asianChartData, setAsianChartData] = useState([]);
 
     useEffect(() => {
         fetchData(fetchCountryData);
@@ -138,13 +143,31 @@ function App() {
 
                     setPatientDataUpdatedAt(moment(updatedAt, "YYYY-M-DD", true).format("YYYY-MM-DD"));
                     setPatientChartData(chartData);
+
+                    const processedAsianData = [];
+
+                    ASIAN_COUNTRIES.map(country => {
+
+                        const lastIndex = val[country].length - 1;
+                        const countryData = val[country][lastIndex];
+
+                        const tempActive = {};
+                        tempActive["country"] = country;
+                        tempActive["type"] = "Confirmed cases";
+                        tempActive["value"] = countryData.confirmed;
+                        processedAsianData.push(tempActive);
+
+                        return countryData;
+                    });
+
+                    setAsianChartData([...processedAsianData]);
+                    callback();
+
                 }).catch(_ => setError(true));
 
             setHospitalData([...hospitalData, ...data.hospital_data]);
             setIsLoading(false);
             setError(false);
-
-            callback();
         } catch(error) {
             setError(true);
         }
@@ -158,6 +181,20 @@ function App() {
         const val = formatNumber(value);
         return val + " " + text;
     };
+
+    function onChangeChart(value) {
+        setIsAsia(value.target.value);
+    }
+
+    function onChangeCountry(countries) {
+        defaultCounties.forEach(element => {
+            filteredCountries.push(element);
+        });
+        countries.forEach(element => {
+            filteredCountries.push(element);
+        });
+        setFilteredCountries([filteredCountries]);
+    }
 
     const cases = {
         text: TOTAL_CASES,
@@ -189,6 +226,47 @@ function App() {
         other: otherData
     };
 
+    const patientChartConf = {
+        config: {
+            title: {
+                visible: false
+            },
+            description: {
+                visible: false
+            },
+            padding: "auto",
+            forceFit: true,
+            point: {
+                visible: true,
+                size: 4
+            },
+            responsive: true,
+            smooth: true,
+            chartData: isAsia ? asianChartData : patientChartData,
+            label: {
+                visible: isAsia ? true : true,
+                offset: 20,
+                type: "point"
+            },
+            lineChart: !isAsia,
+            xField: isAsia ? "value" : "date",
+            yField: isAsia ? "country" : "confirmed",
+            stackField: isAsia ? "type" : "",
+            yAxis: {
+                tickCount: isAsia ? 18 : 10
+            },
+            xAxis: {
+                tickCount: isAsia ? 10 : 10
+            },
+            height: isAsia ? 500 : 400,
+            style: {
+                width: "100%"
+            }
+        },
+        countries: [defaultCounties, filteredCountries],
+        onChange: [onChangeChart, onChangeCountry]
+    };
+
     return (
         <Layout className="layout">
             <BackTop/>
@@ -207,7 +285,7 @@ function App() {
                         </Row>
                         {!isErrorCountryData && <div>
                             <Row justify="space-around">
-                                <ChartPanel ageChartData={ageChartData} patientChartData={patientChartData}
+                                <ChartPanel ageChartData={ageChartData} patientChartData={patientChartConf}
                                             hospitalData={hospitalData}
                                             patientDataUpdatedAt={patientDataUpdatedAt}
                                             ageDataUpdatedAt={countryDataUpdatedDate} genderChartData={byGenderData}/>
