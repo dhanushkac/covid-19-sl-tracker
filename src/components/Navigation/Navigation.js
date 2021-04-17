eabee9add71ee815ac77f320debb899632b3c195
Dhanushka C <dhanushka787@gmail.com>
2021-04-17 11:05:47 +0530

    Update header and antd version

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { BackTop, Col, Layout, Radio, Row, Typography } from "antd";
+import { BackTop, Col, Layout, Radio, Row, Typography, Grid } from "antd";
 
 import HomeIcon from "../../resources/virus.svg";
 
 const { Header } = Layout;
 const { Title } = Typography;
+const { useBreakpoint } = Grid;
 
 const Navigation = () => {
 
@@ -12,15 +13,20 @@ const Navigation = () => {
     console.log(e);
   };
 
+  const screens = useBreakpoint();
+  const isMobile = !screens.md && screens.xs;
+
   return <>
     <BackTop/>
     <Header>
       <Row style={{ width: '100%' }}>
-        <Col span={12} style={{ display: 'flex', alignItems: 'center' }}>
+        <Col xs={{ span: 12 }} sm={{ span: 12 }} md={{ span: 16 }}
+             lg={{ span: 12 }} xl={{ span: 12 }} style={{ display: 'flex', alignItems: 'center' }}>
           <img src={HomeIcon} alt="" width={50}/>
-          <Title level={3} className="logo">COVID-19 Sri Lanka Tracker</Title>
+          {!isMobile && <Title level={4} className="logo">COVID-19 Sri Lanka Tracker</Title>}
         </Col>
-        <Col span={12} style={{ textAlign: 'right' }}>
+        <Col xs={{ span: 12 }} sm={{ span: 12 }} md={{ span: 8 }}
+             lg={{ span: 12 }} xl={{ span: 12 }} style={{ textAlign: 'right' }}>
           <Radio.Group
             onChange={onChange}
             defaultValue={'en'}
